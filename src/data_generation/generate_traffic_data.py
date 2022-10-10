17f1e021a27c14e2b07b9f696a1a0e5cfe427443
eqsmy <elysiaqs@fenway.localdomain>
2022-10-10 13:26:06 -0700

    settings.py progress

--- src/data_generation/generate_traffic_data.py
+++ src/data_generation/generate_traffic_data.py
@@ -1,7 +1,4 @@
-# DONE: git ignore the datasets / extra files
-# TODO: config file for parameters
-
-import settings
+from settings import *
 from xpc3 import *
 from xpc3_helper import *
 from PIL import Image
@@ -10,23 +7,41 @@ import time
 import matplotlib.pyplot as plt
 import mss
 import cv2
-import os
 import pymap3d as pm
-import pandas as pd
 
 def rads_to_degs(degs):
     return degs * (np.pi / 180)
 
+
 def set_position(client, ac, e, n, u, psi, pitch=-998, roll=-998):
-    ref = settings.REGION_OPTIONS[settings.REGION_CHOICE]
-    p = pm.enu2geodetic(e, n, u, ref[0], ref[1], ref[2]) #east, north, up, 
-    client.sendPOSI([*p, pitch, roll, psi], ac) #psi=ya (heading)
+    """Sets position of aircraft in XPlane
+
+    Parameters
+    ----------
+    client : SocketKind.SOCK_DGRAM
+        XPlaneConnect socket
+    ac : int
+        number of aircraft to position
+    e : float
+        eastward distance from origin location in meters
+    n : float
+        northward distance from origin location in meters
+    u : float
+        upward distance from origin location in meters
+    psi : float
+        aircraft heading in degrees
+    pitch : int (optional)
+    roll : int (optional)
+    """
+    ref = REGION_OPTIONS[REGION_CHOICE]
+    p = pm.enu2geodetic(e, n, u, ref[0], ref[1], ref[2]) #east, north, up
+    client.sendPOSI([*p, pitch, roll, psi], ac)
 
 
-def get_intruder_position(e0, n0, u0, h0, z, hang, vang):
-    e1 = z * np.tan(rads_to_degs(hang))
+def get_intruder_position(e0, n0, u0, h0, z, hang, vang, p0, r0):
+    e1 = z * np.tan(rads_to_degs(hang + r0))
     n1 = z
-    u1 = z * np.tan(rads_to_degs(vang))
+    u1 = z * np.tan(rads_to_degs(vang + p0))
 
     # Rotate
     n1 = (z / np.cos(rads_to_degs(hang))) * \
@@ -41,46 +56,53 @@ def get_intruder_position(e0, n0, u0, h0, z, hang, vang):
 
     return e1, n1, u1
 
+def random_normal(min, max, loc, scale):
+    out = np.random.normal(loc, scale)
+    while out > max or out < min:
+        out = np.random.normal(loc, scale)
+    return out
+
 def sample_random_state():
     # Ownship state
-    e0 = np.random.uniform(-settings.EAST_RANGE, settings.EAST_RANGE)  # m
-    n0 = np.random.uniform(-settings.NORTH_RANGE, settings.NORTH_RANGE)  # m
-    u0 = np.random.uniform(-settings.UP_RANGE, settings.UP_RANGE)  # m
-    h0 = np.random.uniform(0.0, 360.0)  # degrees
+    e0 = np.random.uniform(-EAST_RANGE, EAST_RANGE)  # meters
+    n0 = np.random.uniform(-NORTH_RANGE, NORTH_RANGE)  # meters
+    u0 = np.random.uniform(-UP_RANGE, UP_RANGE)  # meters
+    h0 = np.random.uniform(OWNSHIP_HEADING[0], OWNSHIP_HEADING[1])  # degrees
+    p0 = random_normal(PITCH_RANGE[0], PITCH_RANGE[1], 0, 30) # degrees
+    r0 = random_normal(ROLL_RANGE[0], ROLL_RANGE[1], 0, 30) # degrees
 
     # Info about relative position of intruder
-    vang = np.random.uniform(-25.0, 25.0)  # degrees
-    hang = np.random.uniform(-38.0, 38.0)  # degrees
-    z = np.random.uniform(20, 2000)  # meters
+    vang = np.random.uniform(VANG_RANGE[0], VANG_RANGE[1])  # degrees
+    hang = np.random.uniform(HANG_RANGE[0], HANG_RANGE[1])  # degrees
+    z = np.random.uniform(DIST_RANGE[0], DIST_RANGE[1])  # meters
     # while z < 20.0:
     #     z = np.random.gamma(2, 200)  # meters
 
     # Intruder state
-    e1, n1, u1 = get_intruder_position(e0, n0, u0, h0, z, hang, vang)
-    h1 = np.random.uniform(0.0, 360.0)  # degrees
+    e1, n1, u1 = get_intruder_position(e0, n0, u0, h0, z, hang, vang, p0, r0)
+    h1 = np.random.uniform(INTRUDER_HEADING[0], INTRUDER_HEADING[1])  # degrees
 
-    return e0, n0, u0, h0, vang, hang, z, e1, n1, u1, h1
+    return e0, n0, u0, h0, p0, r0, vang, hang, z, e1, n1, u1, h1
 
-def gen_data(client, npoints, outdir):
+def gen_data(client, outdir):
     screen_shot = mss.mss()
     csv_file = outdir + 'state_data.csv'
-    print("0")
     with open(csv_file, 'w') as fd:
         fd.write("filename,e0,n0,u0,h0,vang,hang,z,e1,n1,u1,h1\n")
 
-    for i in range(npoints):
+    for i in range(NUM_SAMPLES):
         # Sample random state
-        e0, n0, u0, h0, vang, hang, z, e1, n1, u1, h1 = sample_random_state()
+        e0, n0, u0, h0, p0, r0, vang, hang, z, e1, n1, u1, h1 = sample_random_state()
 
         # Position the aircraft
-        #client.sendDREF("sim/time/zulu_time_sec", 9.0*3600 + 8*3600)
-        zulu_time = np.random.uniform(settings.TIME_OF_DAY_START * 3600, settings.TIME_OF_DAY_END * 3600)
+        zulu_time = TIME_OPTIONS[REGION_CHOICE] * 3600
+        zulu_time += np.random.uniform(TIME_OF_DAY_START * 3600, TIME_OF_DAY_END * 3600)
         client.sendDREF("sim/time/zulu_time_sec", zulu_time)
-        set_position(client, 0, e0, n0, u0, h0)
+        set_position(client, 0, e0, n0, u0, h0, p0, r0)
         set_position(client, 1, e1, n1, u1, h1)
 
         # Pause and then take the screenshot
-        time.sleep(0.05)
+        time.sleep(PAUSE_2)
         ss = np.array(screen_shot.grab(screen_shot.monitors[0]))[12:-12, :, :]
 
         # Deal with screen tearing
@@ -104,7 +126,7 @@ def gen_data(client, npoints, outdir):
             fd.write("%d,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f\n" %
                      (i, e0, n0, u0, h0, vang, hang, z, e1, n1, u1, h1))
 
-
+'''
 def gen_data_from_states(client, state_file, outdir):
     states = pd.read_csv(state_file)
     
@@ -143,7 +165,7 @@ def gen_data_from_states(client, state_file, outdir):
         with open(csv_file, 'a') as fd:
             fd.write("%d,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f\n" %
                      (i, states['e0'][i], states['n0'][i], states['u0'][i], states['h0'][i], states['vang'][i], states['hang'][i], states['z'][i], states['e1'][i], states['n1'][i], states['u1'][i], states['h1'][i]))
-
+'''
 
 client = XPlaneConnect()
 client.pauseSim(True)
@@ -151,12 +173,6 @@ client.sendDREF("sim/operation/override/override_joystick", 1)
 set_position(client, 1, 0, 1200, 10, 90, roll=0, pitch=0)
 set_position(client, 0, 0, 1200, 10, 90, roll=0, pitch=0)
 
-npoints = 10
+time.sleep(PAUSE_1)
 
-time.sleep(3)
-
-#state_file = "/home/smkatz/Documents/RiskSensitivePerception/collision_avoidance/data_files/risk_data_states_v3.csv"
-
-#gen_data_from_states(client, state_file, outdir)
-#np.random.seed(3)
-gen_data(client, npoints, settings.OUTDIR)
+gen_data(client, OUTDIR)
--- src/data_generation/settings.py
+++ src/data_generation/settings.py
@@ -1,14 +1,10 @@
 
 OUTDIR = "/Users/elysiasmyers/Desktop/STANFORD/VisionBasedAircraftDAA/datasets/"
+OUTDIR = "C:/Users/X-Plane/Desktop/VisionBasedAircraftDAA/datasets/"
 
 REGION_CHOICE = "Palo Alto"
 
-# Latitude, Longitiude, and Altitude of different region locations
-REGION_OPTIONS = {
-    "Palo Alto": [37.46358871459961, -122.11750030517578, 1578.909423828125]
-}
-
-# OWNSHIP LOCATION
+# OWNSHIP POSITION
 # Used to determine location of Ownship from region location
 # Will be used to determine distance from location in meters in positive or negative direction
 # Entered as a float (i.e. with a decimal) in meters >= 0
@@ -16,9 +12,48 @@ EAST_RANGE = 5000.0
 NORTH_RANGE = 5000.0
 UP_RANGE = 500.0
 
+# OWNSHIP POSITION Cont. 
+# all in (min, max) form
+OWNSHIP_HEADING = (0.0, 360.0) # degrees
+PITCH_RANGE = (-90, 90) # degrees (-90 = straight down, 90 = straight up)
+ROLL_RANGE = (-90, 90) # degrees (-90 = full roll left, 90 = full roll right)
+
+# INTRUDER POSITION
+# all in (min, max) form
+INTRUDER_HEADING = (0.0, 360.0) # degrees
+VANG_RANGE = (-25.0, 25.0) # degrees, vertical angle from ownship
+HANG_RANGE = (-38.0, 38.0) # degrees, horizontal angle from ownship
+DIST_RANGE = (20, 2000) # meters, diagonal distance from ownship
+
 # TIME OF DAY
 # Start and end of range of possible time of day in local time, e.g. 8.0 = 8AM, 17.0 = 5PM
 # For each sinsoidal trajectory in the run, the time of day will be sampled uniformly
 # from this range
 TIME_OF_DAY_START = 8.0
-TIME_OF_DAY_END = 11.0
+TIME_OF_DAY_END = 17.0
+
+# CLOUD COVER
+# (higher numbers are cloudier/darker)
+# 0 = Clear, 1 = Cirrus, 2 = Scattered, 3 = Broken, 4 = Overcast
+CLOUD_COVER = 4
+
+# SAMPLING
+# Number of samples desired for dataset
+NUM_SAMPLES = 10
+
+
+
+'''------CONSTANTS BELOW THIS LINE (Unlikely to need changing)-------'''
+# Seconds to pause
+PAUSE_1 = 3 # before data generation
+PAUSE_2 = 0.05 # between each sample
+
+# Latitude, Longitiude, and Altitude of different region locations
+REGION_OPTIONS = {
+    "Palo Alto": [37.46358871459961, -122.11750030517578, 1578.909423828125]
+}
+
+# Hours behind Zulu (UTC) time for each region
+TIME_OPTIONS = {
+    "Palo Alto": 8
+}

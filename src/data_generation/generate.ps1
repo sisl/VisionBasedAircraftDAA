dad1d1b10f73f832f0363334f635a8189dd5f658
eqsmy <elysiaqs@fenway.localdomain>
2023-01-17 12:59:57 -0800

    prep for data gen

--- src/data_generation/generate.ps1
+++ src/data_generation/generate.ps1
@@ -2,36 +2,38 @@
 # and the end of the block is indicated by '#>'. Use '#' in front of the '<#' and '#>' to uncomment the 
 # commands in between.
 
+$global:NAME="starter_dataset"
+
 <#TESTING
-python -m generate_traffic_data -aw -ac "Cessna Skyhawk" --newac --name "test"
+python -m generate_traffic_data -aw -ac "Cessna Skyhawk" --newac --name $NAME
 #>
 
 <#CessnaSkyhawk
-python -m generate_traffic_data -aw --train 900 --valid 100 --location "Palo Alto" --name "sample_dataset" -ac "Cessna Skyhawk" --newac
+python -m generate_traffic_data -aw --train 900 --valid 100 --location "Palo Alto" --name $NAME -ac "Cessna Skyhawk" --newac
 if ( $? ) 
 {
-    python -m generate_traffic_data -aw --train 900 --valid 100 --location "Osh Kosh" --name "sample_dataset" -ac "Cessna Skyhawk" --append
-    python -m generate_traffic_data -aw --train 900 --valid 100 --location "Boston" --name "sample_dataset" -ac "Cessna Skyhawk" --append
-    python -m generate_traffic_data -aw --train 900 --valid 100 --location "Reno Tahoe" --name "sample_dataset" -ac "Cessna Skyhawk" --append
+    python -m generate_traffic_data -aw --train 900 --valid 100 --location "Osh Kosh" --name $NAME -ac "Cessna Skyhawk" --append
+    python -m generate_traffic_data -aw --train 900 --valid 100 --location "Boston" --name $NAME -ac "Cessna Skyhawk" --append
+    python -m generate_traffic_data -aw --train 900 --valid 100 --location "Reno Tahoe" --name $NAME -ac "Cessna Skyhawk" --append
 }
 #>
 
 <#Boeing737-800
-python -m generate_traffic_data -aw --train 9 --valid 1 --location "Palo Alto" --name "sample_dataset" -ac "Boeing 737-800" --append --newac
+python -m generate_traffic_data -aw --train 900 --valid 100 --location "Palo Alto" --name $NAME -ac "Boeing 737-800" --append --newac
 if ( $? ) 
 {
-    python -m generate_traffic_data -aw --train 900 --valid 100 --location "Osh Kosh" --name "sample_dataset" -ac "Boeing 737-800" --append
-    python -m generate_traffic_data -aw --train 900 --valid 100 --location "Boston" --name "sample_dataset" -ac "Boeing 737-800" --append
-    python -m generate_traffic_data -aw --train 900 --valid 100 --location "Reno Tahoe" --name "sample_dataset" -ac "Boeing 737-800" --append
+    python -m generate_traffic_data -aw --train 900 --valid 100 --location "Osh Kosh" --name $NAME -ac "Boeing 737-800" --append
+    python -m generate_traffic_data -aw --train 900 --valid 100 --location "Boston" --name $NAME -ac "Boeing 737-800" --append
+    python -m generate_traffic_data -aw --train 900 --valid 100 --location "Reno Tahoe" --name $NAME -ac "Boeing 737-800" --append
 }
 #>
 
 <#KingAirC90
-python -m generate_traffic_data -aw --train 9 --valid 1 --location "Palo Alto" --name "sample_dataset" -ac "King Air C90" --append --newac
+python -m generate_traffic_data -aw --train 900 --valid 100 --location "Palo Alto" --name $NAME -ac "King Air C90" --append --newac
 if ( $? ) 
 {
-    python -m generate_traffic_data -aw --train 900 --valid 100 --location "Osh Kosh" --name "sample_dataset" -ac "King Air C90" --append
-    python -m generate_traffic_data -aw --train 900 --valid 100 --location "Boston" --name "sample_dataset" -ac "King Air C90" --append
-    python -m generate_traffic_data -aw --train 900 --valid 100 --location "Reno Tahoe" --name "sample_dataset" -ac "King Air C90" --append
+    python -m generate_traffic_data -aw --train 900 --valid 100 --location "Osh Kosh" --name $NAME -ac "King Air C90" --append
+    python -m generate_traffic_data -aw --train 900 --valid 100 --location "Boston" --name $NAME -ac "King Air C90" --append
+    python -m generate_traffic_data -aw --train 900 --valid 100 --location "Reno Tahoe" --name $NAME -ac "King Air C90" --append
 }
 #>
